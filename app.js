ecb437b0fbcc82c6b7409698b732295b155e00b4
José Gordillo Panighini <63110718+josegordillo791@users.noreply.github.com>
2021-08-26 22:51:04 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,28 +2,40 @@ function  ActualizarHora () {
        var fecha = new Date(),
            horas = fecha.getHours(),
            ampm=ampm,
+           diadelmes=fecha.getDate();
            min=fecha.getMinutes(),
            seg=fecha.getSeconds(),
+           anio=fecha.getFullYear(),
            dia=fecha.getDay(),
            mes=fecha.getMonth(); 
-          // year=fecha.getfullYear();
+           
 
-           var semana = ['Domingo',"Lunes" ,"Martes" ,"Miercoles","Jueves" ,"Sabado"];
-           var meses_a = ["Enero","Febrero" ,"Marzo" ,"Abril" ,"Mayo" ,"Junio" ,"Julio" ,"Agosto" ,"Septiembre" ,"Octubre" ,"Noviembre" ,"Diciembre"]; 
+           var semana_a =  ['Domingo',"Lunes" ,"Martes" ,"Miercoles","Jueves" ,"Sabado"];
+           var meses_a =  ["Enero","Febrero" ,"Marzo" ,"Abril" ,"Mayo" ,"Junio" ,"Julio" ,"Agosto" ,"Septiembre" ,"Octubre" ,"Noviembre" ,"Diciembre"]; 
            
           /*  let diasemana=document.getElementById("diaSemana"); */
+        
+            
 
+ 
 
-              
+              var diaDeSemana = semana_a[dia];
+              console.log("DIA DE SEMANA ES :"+diaDeSemana);      
+              var parrafo =document.getElementById("3"); 
+              parrafo.textContent=diaDeSemana+" "+diadelmes+" de "+ meses_a[mes]+" del "+  anio;
     
+
+      
    }   
 
    ActualizarHora();
    
    var seteo = setInterval (ActualizarHora,1000);
 
-   var x = "x";
-   parrafo =document.getElementById("3");
-   parrafo.innerHTML="hh";
+   
+   
+   
+
+  
 
 
